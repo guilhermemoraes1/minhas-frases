8cbdcf6fa1245a1a5a3e84634560b58cd9d42837
Guilherme Moraes <127672759+guilhermemoraes1@users.noreply.github.com>
2023-04-06 14:14:40 -0300

    organizando meu codigo

---
+++ index.py
@@ -0,0 +1,33 @@
+import random # importa o módulo random para gerar números pseudo-aleatórios
+
+# minhas frases armazenadas em uma lista
+frases = ['0', '1', '2', '3', 4]
+
+while True:
+  print(f"{'*'*70}")
+
+  # esse while da ao user a opção de escolher de que forma ele quer acessar o elemento
+  while True:
+    print('Você quer exibir uma frase pelo índice, pelo modo aleatório ou saber a quantidade de frases?(0/1/2)')
+    option = input()
+    if option == '0':
+      indice = int(input('\nDigite o índice abaixo:\n'))
+      print(f'\n{frases[indice]}\n')
+      break
+    elif option == '1':
+      print(f'\n{frases[random.randrange(0, len(frases))]}\n')
+      break
+    elif option == '2':
+      print(f'\nO programa possui um total de {len(frases)} frases/poemas.\n')
+      break
+  
+  # esse while pergunta ao user se ele quer continuar usando o programa
+  while True:
+    saida = input('Você deseja continuar?(S/n)\n').lower()
+    if saida == 'n':
+      print(f"{'*'*70}")
+      break
+    elif saida == 's':
+      break
+  if saida == 'n':
+    break
